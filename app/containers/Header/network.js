53a4d572afbb68a5dbe847df25776b0e880b0df1
Angelia <greenfish84@126.com>
2019-04-11 16:11:21 +0800

    modify nginx to resolve https block

--- app/containers/Header/network.js
+++ app/containers/Header/network.js
@@ -2,7 +2,10 @@ const Network = {
   Offline: { rpc: 'offline', tx_explorer: null },
   'Local RPC': { rpc: 'http://localhost:8091', tx_explorer: null },
   Testnet: {
-    rpc: 'http://testnet-jsonrpc.conflux-chain.org:12537',
+    rpc:
+      process.env.NODE_ENV === 'production'
+        ? 'https://wallet.confluxscan.io/api'
+        : 'http://testnet-jsonrpc.conflux-chain.org:12537',
     tx_explorer: 'http://confluxscan.io/tx/',
   },
   // 'Main Net': { rpc: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://etherscan.io/tx/' },
