b9872f1c226a608559a9c836cdcd5746048335f4
winljm001 <819648983@qq.com>
2019-07-06 23:35:08 +0800

    头部导航，缺响应式

--- src/pages/layout/sider-menus/index.tsx
+++ src/pages/layout/sider-menus/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import IconButton from '@material-ui/core/IconButton'
 import MenuItem from '@material-ui/core/MenuItem'
-import Menu from '@material-ui/core/Menu'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import MenuList from '@material-ui/core/MenuList'
+import Grow from '@material-ui/core/Grow'
+import Paper from '@material-ui/core/Paper'
+import Popper from '@material-ui/core/Popper'
+import styles from './style.module.scss'
+import Images from '@/assets/images/index'
+import Hidden from '@material-ui/core/Hidden'
 export default function MenuAppBar() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
-
+  const anchorRef = React.useRef(null)
   function handleMenu(event: React.MouseEvent<HTMLElement>) {
     setAnchorEl(event.currentTarget)
   }
@@ -16,33 +22,49 @@ export default function MenuAppBar() {
 
   return (
     <div>
-      <IconButton
-        aria-label="Account of current user"
-        aria-controls="menu-appbar"
-        aria-haspopup="true"
-        onClick={handleMenu}
-        color="inherit"
-      >
-        acc
-      </IconButton>
-      <Menu
-        id="menu-appbar"
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-      </Menu>
+      <Hidden xsDown>
+        <div className={styles.operateListPc}>
+          <div className={styles.operateListPcItem}>
+            <img src={Images.pc} alt="" className={styles.icon} />
+            Deploy
+          </div>
+          <div className={styles.operateListPcItem}>
+            <img src={Images.f5} alt="" className={styles.icon} />
+            Refresh
+          </div>
+          <div className={styles.operateListPcItem}>
+            <span
+              aria-label="Account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+            >
+              Testnet
+              <img src={Images.down} alt="" className={styles.triangleIcon} />
+            </span>
+            <Popper open={open} anchorEl={anchorRef.current} transition disablePortal>
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+                  }}
+                >
+                  <Paper id="menu-list-grow">
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList>
+                        <MenuItem onClick={handleClose}>Testnet</MenuItem>
+                        <MenuItem onClick={handleClose}>Local RPC</MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </div>
+        </div>
+      </Hidden>
+      <Hidden smUp>smUp</Hidden>
     </div>
   )
 }
--- src/pages/layout/top-header/operate-list/index.tsx
+++ src/pages/layout/top-header/operate-list/index.tsx
@@ -1,70 +1,197 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styles from './style.module.scss'
+import Images from '@/assets/images/index'
 import MenuItem from '@material-ui/core/MenuItem'
 import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 import MenuList from '@material-ui/core/MenuList'
 import Grow from '@material-ui/core/Grow'
 import Paper from '@material-ui/core/Paper'
 import Popper from '@material-ui/core/Popper'
-import styles from './style.module.scss'
-import Images from '@/assets/images/index'
 import Hidden from '@material-ui/core/Hidden'
-export default function MenuAppBar() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const anchorRef = React.useRef(null)
-  function handleMenu(event: React.MouseEvent<HTMLElement>) {
-    setAnchorEl(event.currentTarget)
-  }
-
-  function handleClose() {
-    setAnchorEl(null)
-  }
-
-  return (
-    <div>
-      <Hidden xsDown>
-        <div className={styles.operateListPc}>
-          <div className={styles.operateListPcItem}>
-            <img src={Images.pc} alt="" className={styles.icon} />
-            Deploy
-          </div>
-          <div className={styles.operateListPcItem}>
-            <img src={Images.f5} alt="" className={styles.icon} />
-            Refresh
-          </div>
-          <div className={styles.operateListPcItem}>
-            <span
-              aria-label="Account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-            >
-              Testnet
-              <img src={Images.down} alt="" className={styles.triangleIcon} />
-            </span>
-            <Popper open={open} anchorEl={anchorRef.current} transition disablePortal>
-              {({ TransitionProps, placement }) => (
-                <Grow
-                  {...TransitionProps}
-                  style={{
-                    transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
-                  }}
-                >
-                  <Paper id="menu-list-grow">
-                    <ClickAwayListener onClickAway={handleClose}>
-                      <MenuList>
-                        <MenuItem onClick={handleClose}>Testnet</MenuItem>
-                        <MenuItem onClick={handleClose}>Local RPC</MenuItem>
-                      </MenuList>
-                    </ClickAwayListener>
-                  </Paper>
-                </Grow>
-              )}
-            </Popper>
-          </div>
-        </div>
-      </Hidden>
-      <Hidden smUp>smUp</Hidden>
-    </div>
-  )
+// interface LangItem {
+//   img: any
+//   lang: string
+//   field: string
+// }
+const netList = ['Testnet', 'Local RPC']
+const LangList = [
+  {
+    img: Images.chinese,
+    lang: 'Chinese',
+    field: 'CHS',
+  },
+  {
+    img: Images.yingyu,
+    lang: 'English',
+    field: 'EN',
+  },
+]
+interface IState {
+  menuNet: null | HTMLElement
+  menuNetSelected: string
+  menuLang: null | HTMLElement
+  menuLangSelected: number
 }
+class OperateList extends Component<{}, IState> {
+  state = {
+    menuNet: null,
+    menuNetSelected: 'Testnet',
+    menuLang: null,
+    menuLangSelected: 0,
+  }
+  handleMenu(event: React.MouseEvent<HTMLElement>, type: string) {
+    if (type === 'menuNet') {
+      this.setState({
+        menuNet: event.currentTarget,
+      })
+    }
+    if (type === 'menuLang') {
+      this.setState({
+        menuLang: event.currentTarget,
+      })
+    }
+  }
+  handleClose(type: string) {
+    if (type === 'menuNet') {
+      this.setState({
+        menuNet: null,
+      })
+    }
+    if (type === 'menuLang') {
+      this.setState({
+        menuLang: null,
+      })
+    }
+  }
+  selectMenu(type: string, val: any) {
+    if (type === 'menuNet') {
+      this.setState({
+        menuNetSelected: val,
+      })
+    }
+    if (type === 'menuLang') {
+      this.setState({
+        menuLangSelected: val,
+      })
+    }
+    this.handleClose(type)
+  }
+  render() {
+    const { menuNet, menuNetSelected, menuLang, menuLangSelected } = this.state
+    const menuNetOpen = Boolean(menuNet)
+    const menuLangOpen = Boolean(menuLang)
+    return (
+      <div>
+        <Hidden xsDown>
+          <div className={styles.operateListPc}>
+            <div className={styles.operateListPcItem}>
+              <img src={Images.pc} alt="" className={styles.icon} />
+              Deploy
+            </div>
+            <div className={styles.operateListPcItem}>
+              <img src={Images.f5} alt="" className={styles.icon} />
+              Refresh
+            </div>
+            <div className={styles.operateListPcItem}>
+              <span
+                aria-label="Account of current user"
+                aria-controls="menuNet"
+                aria-haspopup="true"
+                onClick={e => {
+                  this.handleMenu(e, 'menuNet')
+                }}
+              >
+                {menuNetSelected}
+                <img src={Images.down} alt="" className={styles.triangleIcon} />
+              </span>
+              <Popper open={menuNetOpen} anchorEl={menuNet} transition disablePortal>
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{
+                      transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+                    }}
+                  >
+                    <Paper id="menuNet">
+                      <ClickAwayListener
+                        onClickAway={() => {
+                          this.handleClose('menuNet')
+                        }}
+                      >
+                        <MenuList>
+                          {netList.map((v, i) => {
+                            return (
+                              <MenuItem
+                                key={i}
+                                selected={v === menuNetSelected}
+                                onClick={() => {
+                                  this.selectMenu('menuNet', v)
+                                }}
+                              >
+                                {v}
+                              </MenuItem>
+                            )
+                          })}
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
+            </div>
+            <div className={styles.operateListPcItem}>
+              <span
+                aria-label="Account of current user"
+                aria-controls="menuLang"
+                aria-haspopup="true"
+                onClick={e => {
+                  this.handleMenu(e, 'menuLang')
+                }}
+              >
+                <img src={LangList[menuLangSelected].img} alt="" className={styles.countryIcon} />
+                {LangList[menuLangSelected].field}
+                <img src={Images.down} alt="" className={styles.triangleIcon} />
+              </span>
+              <Popper open={menuLangOpen} anchorEl={menuLang} transition disablePortal>
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{
+                      transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+                    }}
+                  >
+                    <Paper id="menuLang">
+                      <ClickAwayListener
+                        onClickAway={() => {
+                          this.handleClose('menuLang')
+                        }}
+                      >
+                        <MenuList>
+                          {LangList.map((v, i) => {
+                            return (
+                              <MenuItem
+                                key={i}
+                                selected={i === menuLangSelected}
+                                onClick={() => {
+                                  this.selectMenu('menuLang', i)
+                                }}
+                              >
+                                {v.lang}
+                              </MenuItem>
+                            )
+                          })}
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
+            </div>
+          </div>
+        </Hidden>
+        <Hidden smUp>smUp</Hidden>
+      </div>
+    )
+  }
+}
+export default OperateList
