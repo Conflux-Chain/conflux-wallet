704a78dd130fa3e91ef11494addcf92984b25fce
李佳明 <lijiaming@iwubida.com>
2019-07-09 19:11:06 +0800

    添加tips

--- src/pages/layout/sider-menus/content/operation-fuzhi/index.tsx
+++ src/pages/layout/sider-menus/content/operation-fuzhi/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Tooltip from '@material-ui/core/Tooltip'
 import styles from './style.module.scss'
 import Images from '@/assets/images/index'
 
@@ -6,9 +7,11 @@ class Operation extends Component {
   clickHandle() {}
   render() {
     return (
-      <div className={styles.operationWrap}>
-        <img src={Images.fuzhi} alt="" className={styles.icon} onClick={this.clickHandle} />
-      </div>
+      <Tooltip title="Copy address to clipboard">
+        <div className={styles.operationWrap}>
+          <img src={Images.fuzhi} alt="" className={styles.icon} onClick={this.clickHandle} />
+        </div>
+      </Tooltip>
     )
   }
 }
