fa3f8b5386ef3678194860be95fd2da7be0d249f
李佳明 <lijiaming@iwubida.com>
2019-07-09 21:40:50 +0800

    Private Key

--- src/pages/layout/sider-menus/content/operation-daochu/index.tsx
+++ src/pages/layout/sider-menus/content/operation-daochu/index.tsx
@@ -1,17 +1,75 @@
 import React, { Component } from 'react'
 import Tooltip from '@material-ui/core/Tooltip'
+import Dialog from '@material-ui/core/Dialog'
+import MuiDialogTitle from '@material-ui/core/DialogTitle'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
 import styles from './style.module.scss'
 import Images from '@/assets/images/index'
 
-class Operation extends Component {
-  clickHandle() {}
+interface IState {
+  openDialog?: boolean
+}
+class Operation extends Component<{}, IState> {
+  state = {
+    openDialog: false,
+  }
+  clickHandle() {
+    this.setState({
+      openDialog: true,
+    })
+  }
+  handleClose() {
+    this.setState({
+      openDialog: false,
+    })
+  }
   render() {
+    const { openDialog } = this.state
     return (
-      <Tooltip title="Derive private key">
-        <div className={styles.operationWrap}>
-          <img src={Images.daochu} alt="" className={styles.icon} onClick={this.clickHandle} />
-        </div>
-      </Tooltip>
+      <>
+        <Tooltip title="Derive private key">
+          <div className={styles.operationWrap}>
+            <img
+              src={Images.daochu}
+              alt=""
+              className={styles.icon}
+              onClick={() => {
+                this.clickHandle()
+              }}
+            />
+          </div>
+        </Tooltip>
+        <Dialog
+          onClose={() => {
+            this.handleClose()
+          }}
+          className={styles.dialog}
+          open={openDialog}
+        >
+          <MuiDialogTitle>
+            <h1 className={styles.dialogTitle}>Private Key</h1>
+            <IconButton
+              aria-label="Close"
+              className={styles.dialogCloseBtn}
+              onClick={() => {
+                this.handleClose()
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </MuiDialogTitle>
+          <p className={styles.codeString}>
+            0xba5ea4f15137a9dde2d59d3c1afe0c5655ec8953f21032 5542fbd9b67f1755c0
+          </p>
+          <div className={styles.warnFooter}>
+            <svg className={styles.icon} aria-hidden="true">
+              <use xlinkHref="#iconzhuyi" />
+            </svg>
+            <p>Do not save the private key in screenshot!</p>
+          </div>
+        </Dialog>
+      </>
     )
   }
 }
