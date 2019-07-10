df6fec398da9efb3ff631ebbe7be56fd8e875dd9
funny <736907735@qq.com>
2019-07-10 14:40:41 +0800

    登录模块修改完成

--- src/pages/login/components/restore-wallet/index.tsx
+++ src/pages/login/components/restore-wallet/index.tsx
@@ -1,7 +1,74 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
+import styles from './style.module.scss'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
-export default class RestoreWallet extends Component {
+interface IProps {
+  stepIndex: number
+  uploadFile: () => void
+  checkFile: () => void
+  restorePasswordRight: boolean
+  checkPassword: (password: string) => void
+}
+
+interface IState {
+  password: string | number
+}
+export default class RestoreWallet extends PureComponent<IProps, IState> {
+  state = {
+    password: '',
+  }
+
+  changePassword = (e: any) => {
+    this.setState({
+      password: e.target.value,
+    })
+  }
   render() {
-    return <div>BetterDialog</div>
+    const { stepIndex, uploadFile, checkFile, restorePasswordRight, checkPassword } = this.props
+    const { password } = this.state
+    return (
+      <div className={styles.restoreWallet}>
+        {!stepIndex ? (
+          <div className={styles.container1}>
+            <div className={styles.uploadBox} onClick={uploadFile}>
+              <svg className={styles.uploadIc} aria-hidden="true">
+                <use xlinkHref="#iconwenjian-" />
+              </svg>
+            </div>
+            <p className={styles.tips}>
+              Hello, you should upload your
+              <br /> keystore file firstly.
+            </p>
+            <Button variant="contained" className={styles.loginButton} onClick={checkFile}>
+              Continue
+            </Button>
+          </div>
+        ) : (
+          <div className={styles.container2}>
+            <TextField
+              id="input-adornment-password"
+              variant="standard"
+              type="password"
+              label="Password"
+              value={password}
+              onChange={this.changePassword}
+              error={!restorePasswordRight}
+            />
+            {!restorePasswordRight && (
+              <p className={styles.errorTxt}>Please enter the right password!</p>
+            )}
+            <Button
+              variant="contained"
+              disabled={!password.length}
+              className={styles.loginButton}
+              onClick={() => checkPassword(password)}
+            >
+              Access Wallet
+            </Button>
+          </div>
+        )}
+      </div>
+    )
   }
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,8 +8,7 @@ import { I18NHOC } from '@/utils/tools/react'
 import { I18NProps } from '@/i18n/context'
 import Container from '@material-ui/core/Container'
 import Paper from '@material-ui/core/Paper'
-import RestoreModal from './components/restore-modal'
-import CreatModal from './components/creat-modal'
+import ModalLogin from './components/modal-login'
 
 type IProps = IDispatch &
   I18NProps & {
@@ -17,42 +16,110 @@ type IProps = IDispatch &
   }
 
 interface IState {
-  restoreModal: boolean
-  creatModal: boolean
+  open: boolean
+  type: string
+  stepIndex: number
+  restoreFileRight: boolean
+  restorePasswordRight: boolean
+  downloadSuc: boolean
 }
 
 class Login extends Component<IProps, IState> {
   state = {
-    restoreModal: false,
-    creatModal: false,
+    open: false, // 模态框显隐
+    type: 'creat', // 显示类型
+    stepIndex: 0, // 当前步骤
+    restoreFileRight: true, // 密钥文件验证是否正确
+    restorePasswordRight: true, // 重置钱包密码是否正确
+    downloadSuc: false, // keystore文件下载是否成功
   }
-  toogleRestore = () => {
-    const { restoreModal } = this.state
+  // 模态框显隐
+  toogleModal = (type: string = 'creat') => {
+    const { open } = this.state
     this.setState({
-      restoreModal: !restoreModal,
+      type,
+      open: !open,
+      stepIndex: 0,
+      restoreFileRight: true,
+      restorePasswordRight: true,
+      downloadSuc: false,
     })
   }
-  toogleCreat = () => {
-    const { creatModal } = this.state
+  // 上传登录密钥文件
+  uploadFile = () => {}
+
+  // 密钥文件验证
+  checkFile = () => {
+    // 验证正确
     this.setState({
-      creatModal: !creatModal,
+      stepIndex: 1,
+      restoreFileRight: true,
+    })
+
+    // 验证错误
+    // this.setState({
+    //   restoreFileRight: false,
+    // })
+  }
+
+  // 关闭错误信息提示框
+  colseError = () => {
+    this.setState({
+      restoreFileRight: true,
     })
   }
+
+  // 密码验证
+  checkPassword = (password: string) => {
+    // console.log(password)
+    // 验证通过,进入钱包，保存登录状态
+    // this.setState({
+    //   open: false,
+    // })
+    // 密码错误
+    this.setState({
+      restorePasswordRight: false,
+    })
+  }
+
+  // 根据密码生成keystore文件
+  generateKeystore = (password: string) => {
+    // 生成成功
+    this.setState({
+      stepIndex: 1,
+    })
+  }
+
+  // 下载keystore文件
+  downloadFile = () => {
+    // 下载成功
+    this.setState({
+      downloadSuc: true,
+    })
+  }
+
   render() {
-    const { restoreModal, creatModal } = this.state
+    const {
+      open,
+      type,
+      stepIndex,
+      restoreFileRight,
+      restorePasswordRight,
+      downloadSuc,
+    } = this.state
     return (
       <div className={styles.login}>
         <Container>
           <h2 className={styles.title}>Welcome to Conflux Wallet</h2>
           <div className={styles.conBox}>
-            <Paper className={styles.paper} onClick={this.toogleRestore}>
+            <Paper className={styles.paper} onClick={() => this.toogleModal('restore')}>
               <svg className={classnames(styles.nowIcon, styles.icon1)} aria-hidden="true">
                 <use xlinkHref="#iconwenjian" />
               </svg>
               <h5 className={styles.paperTitle}>Restore Wallet</h5>
               <p className={styles.paperDesc}>If you have an account, please restore your wallet</p>
             </Paper>
-            <Paper className={styles.paper} onClick={this.toogleCreat}>
+            <Paper className={styles.paper} onClick={() => this.toogleModal('creat')}>
               <svg className={classnames(styles.nowIcon, styles.icon2)} aria-hidden="true">
                 <use xlinkHref="#iconxinjian" />
               </svg>
@@ -61,8 +128,23 @@ class Login extends Component<IProps, IState> {
             </Paper>
           </div>
         </Container>
-        <RestoreModal open={restoreModal} onClose={this.toogleRestore} />
-        <CreatModal open={creatModal} onClose={this.toogleCreat} />
+        {/* <RestoreModal open={restoreModal} onClose={this.toogleRestore} />
+        <CreatModal open={creatModal} onClose={this.toogleCreat} /> */}
+        <ModalLogin
+          open={open}
+          type={type}
+          onClose={this.toogleModal}
+          stepIndex={stepIndex}
+          uploadFile={this.uploadFile}
+          checkFile={this.checkFile}
+          restoreFileRight={restoreFileRight}
+          restorePasswordRight={restorePasswordRight}
+          colseError={this.colseError}
+          checkPassword={this.checkPassword}
+          generateKeystore={this.generateKeystore}
+          downloadSuc={downloadSuc}
+          downloadFile={this.downloadFile}
+        />
       </div>
     )
   }
