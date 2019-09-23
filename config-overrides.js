7bb7cfba3a1a59c44a995e471345e61d2bc3ef2f
yqrashawn <namy.19@gmail.com>
2019-09-23 15:00:22 +0800

    Test: now.sh cra build

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const path = require('path')
 const { override, addBabelPlugins, addWebpackResolve, overrideDevServer } = require('customize-cra')
+const fs = require('fs')
 
 const devServerConfig = () => config => {
   return {
@@ -24,7 +25,14 @@ module.exports = {
     ...addBabelPlugins('react-hot-loader/babel'),
     addWebpackResolve({
       alias: { '@': path.resolve(__dirname, 'src') },
-    })
+    }),
+    config => {
+      console.log(config)
+      console.log('--------------------')
+      console.log(config.plugins)
+      fs.writeFileSync('./debug-webpack', JSON.stringify(config))
+      return config
+    }
   ),
   devServer: overrideDevServer(devServerConfig()),
 }
