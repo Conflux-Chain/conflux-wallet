fd2a03698460495526de716aa607d6c9e9a0a205
angelia-yuqi-personal <greenfish84@126.com>
2020-04-11 19:00:28 +0800

    add mainnet feature

--- src/i18n/langs/zh_CN/wallet/fans-coin-details.ts
+++ src/i18n/langs/zh_CN/wallet/fans-coin-details.ts
@@ -1,5 +1,5 @@
 export default {
   detailText:
-    'Fans Coin, 简称FC，由 Conflux基金会主导 设计，与社区成员合作研发，并基于Conflux 测试网运行的社区贡献智能合约。',
+    'Fans Coin, 简称FC，由 Conflux基金会主导 设计，与社区成员合作研发，并基于Conflux 测试网运行的社区贡献智能合约',
   viewMore: '查看更多',
 }
