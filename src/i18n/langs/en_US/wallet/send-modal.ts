04fcbd5fe2018c0940bcd8ceaf151210abc0497d
angelia-yuqi-personal <greenfish84@126.com>
2020-04-13 15:00:29 +0800

    address placeholder

--- src/i18n/langs/en_US/wallet/send-modal.ts
+++ src/i18n/langs/en_US/wallet/send-modal.ts
@@ -7,6 +7,6 @@ export default {
   gdrip: 'Gdrip',
   send: 'Send',
   palceHolder1: 'The available balance is ',
-  palceHolder2: 'Address',
+  palceHolder2: 'Enter the address updated after April 9, 2020',
   formErrorText: 'Please enter the right address!',
 }
