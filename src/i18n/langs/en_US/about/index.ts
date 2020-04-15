6b201bb18531c590fb3eec19393c47725124fad0
angelia-yuqi-personal <greenfish84@126.com>
2020-04-15 18:20:38 +0800

    change about text

--- src/i18n/langs/en_US/about/index.ts
+++ src/i18n/langs/en_US/about/index.ts
@@ -1,21 +1,21 @@
 export default {
   h1: 'About FC',
-  title: 'About Fans Coin (FC)',
+  title: 'Introduction of Fans Coin (FC)',
   conFC:
-    'Fans Coin (FC) is a smart contract designed and used solely on the Conflux Blockchain Testnet. It currently serves for two major purposes,\na. Tokens to reward active contributors\nb. A layer of functional testing for Conflux Testnet\nThe Conflux Foundation appreciates each and every contribution to the community. With FC tokens, the Foundation rewards for constructive efforts in the community, while guarantees a future 1:1 conversion ratio against CFX , the upcoming official cryptocurrency operating on the Conflux Blockchain Mainnet.',
-  firstH2: 'Community Agreements',
+    'Fans Coin, here after refers as FC, is a smart contract built on Conflux testnet. The development of FC was initiated by Conflux Foundation, and the project is a joint effort among Conflux Community members. FC aims to fully incentivize and encourage the power of the community. Moreover, FC records every contribution of each member while conducting a wholesome testing on the exsting testnet.\nFor the purpose of acknowledging the contribution from the communtiy, Conflux Foundation committed a 1:1 swap between FC and CFX after the mainnet is launched. ',
+  firstH2: 'Community Conflux Foundation Commitment',
   secondH2: 'Special Thanks',
-  firstH3: 'Distribution Principle',
-  secondH3: 'Exchange Principle',
-  thirdH3: 'Liquidity Principle',
+  firstH3: 'FC Distribution',
+  secondH3: 'FC Tokenswap',
+  thirdH3: 'FC Design',
   contain1:
-    'FC is primarily designed for recognizing community contribution. The Foundation and community respect every legal and positive effort in building the community. In distributing them, the Conflux Foundation strives to be fair and transparent in every pennyworth of FC.',
+    'FC is primarily designed for recognizing all contributions from the communtiy. Conflux Foundation respects and appraises every legal and positive effort in building the ecosystem, therefore the Foundation strives to be fair and transparent in every pennyworth of FC. All the FC distribution will be announced to the community and open to the public via multiple channles.',
   contain2:
-    'The Conflux Foundation issues FC on the promise of prioritizing the FC-CFX exchange services after the expiration of lock-up periods on Foundation’s ecosystem funds. Both locked and available FC balance are eligible for exchange purpose. Note that the Foundation reserves the right to investigate and reject an exchange request if the FC on the subject is involved in illegal activities or violations of the community code-of-conduct. Details for the exchange process will be released soon. Given that the current estimate launch day of Conflux main network is between 2019.Q4 – 2020.Q1, the FC-CFX exchange service will commence shortly after.',
+    'The Conflux Foundation issues FC on the promise of prioritizing the FC-CFX swap after the expiration of lock-up periods on Foundation’s ecosystem funds. Both locked and available FC balance are eligible for the mainnet tokenswap. Note that the Foundation reserves the right to investigate and reject a swap request if the FC on the subject is involved in illegal activities or violations of the community code-of-conduct. Details for the exchange process will be released soon.',
   contain3:
-    'Community members acquire FC from two sources,\na. incentives from the Conflux Foundation\nb. P2P Transaction\nTo motivate members to contribute more and obtain FC via the former, Conflux Foundation makes special adjustment to the liquidity of FC so that FC obtained not directly from the Foundation is subject to a secondary allocation upon transfer; namely, a certain ratio of the transfer amount will be permanently locked for transfer in order to initiate a transfer.',
+    'Community members acquire FC from two sources,\na. incentives from the Conflux Foundation\nb. P2P Transaction within the Conflux Community\nTo motivate members to contribute more and obtain FC via the former, Conflux Foundation makes special adjustment to the liquidity of FC so that FC obtained not directly from the Foundation is subject to a secondary allocation upon transfer; namely, a certain ratio of the transfer amount will be permanently locked for transfer in order to initiate a transfer.',
   contain4:
-    'Scenario:\nAssume the current circulation ratio is 100, meaning for every 100 FCs transfer, an addition of 100 FCs will be permanently locked for transfer. Upon completing a Conflux bounty task, Alice receives 10 FCs from Conflux Foundation as our gratitude. Meanwhile, she also receives a transfer from Bob that worth of 10 FCs. At this point, Alice owns a total of 20 FCs, and she would like to transfer some FCs to Denise. ',
+    'Scenario:\nAssume the current circulation ratio is 100, meaning for every 100 FCs transfer, an addition of 100 FCs will be permanently locked for transfer. Upon completing a Conflux bounty task, Alice receives 10 FCs from Conflux Foundation as our gratitude. Meanwhile, she also receives a transfer from Bob that worth of 10 FCs. At this point, Alice owns a total of 20 FCs, and she would like to transfer some FCs to Denise.',
   contain5:
     'Special shoutout to Justin. As a member of the Conflux community, Justin has actively participated in FC development from early-stage prototyping through the production and release of FC smart contract.',
 }
